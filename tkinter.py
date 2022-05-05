03187a55706b0f7c33dee7b3258f12d7857855f2
Аэлития <53450654+20Aelita03@users.noreply.github.com>
2022-05-05 11:09:58 +0300

    created new click and balance of reaction
    Дописана функция для уравнений с коэффициентами

--- tkinter.py
+++ tkinter.py
@@ -4,11 +4,18 @@ from tkinter import ttk
 import math
 import sys
 from chempy import Substance
+from chempy import Equilibrium
+from chempy import balance_stoichiometry
 
 def click():
     var = Substance.from_formula(entry.get())
     label.config(text="Your formula: " + var.unicode_name + '\n' + 'Mass: ' + '%.3f' % var.mass)
 
+def clicked():
+    ex,prod=entry.get().split('=')
+    ex,prod=balance_stoichiometry(set(ex.split('+')),set(prod.split('+')))
+    label.config(text='Коэффициенты: '+str(dict(ex))+str(dict(prod)))   
+    
 root = Tk()
 
 root.title("Chemical calculator")
@@ -25,5 +32,8 @@ label.pack()
 
 button = Button(frame, text='Посчитать', command=click)
 button.pack()
+
+button2 = Button(frame, text='Уравнять реакцию', command=clicked)
+button2.pack()
 
 root.mainloop()
