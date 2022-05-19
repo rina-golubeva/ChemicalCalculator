cd59116df32a48874862339a0113d8aa8313ece7
Arina Golubeva <99752356+rina-golubeva@users.noreply.github.com>
2022-05-19 12:13:07 +0300

    added comments
    added comments for functions

--- tkinter.py
+++ tkinter.py
@@ -3,14 +3,32 @@ from tkinter import messagebox
 from tkinter import ttk
 import math
 import sys
+
 from chempy import Substance
 from chempy import Equilibrium
 from chempy import balance_stoichiometry
+
+'''
+Function for calculating the molar mass of a substance.
+For a substance entered from the keyboard via the mass method
+from the champy package, the mass value is calculated.
+The substance itself is processed using the Substance function.from_formula,
+its output is carried out through the unicode_name method.
+'''
 
 def click():
     var = Substance.from_formula(entry.get())
     label.config(text="Your formula: " + var.unicode_name + '\n' + 'Mass: ' + '%.3f' % var.mass)
 
+'''
+Function for calculating coefficients in the equation.
+The entered equation is divided into input and output data
+using the split function.
+Using the balance_stoichiometry function from the champy package,
+coefficients are selected in the equation.
+Dictionaries are used for their output.
+'''    
+    
 def clicked():
     ex,prod=entry.get().split('=')
     ex,prod=balance_stoichiometry(set(ex.split('+')),set(prod.split('+')))
@@ -25,7 +43,6 @@ frame.pack()
 
 entry = Entry(frame, width = 50)
 entry.pack()
-
 
 label = Label(frame)
 label.pack()
